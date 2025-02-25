953e3eae9b306f95de80b3892ede0dd3589d8e59
Dyovana <dyo.menegatti@hotmail.com>
2025-02-24 21:14:23 -0300

    feat: adiciona cabecalho e light dark mode

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="home">
-    <span>oi</span>
+  <div :class="themeClass" class="home">
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+import themeMixin from "@/mixins/themeMixin";
+
 export default {
   name: "ToDo",
+  components: {
+    Header,
+  },
+  mixins: [themeMixin],
 };
 </script>
+
+<style lang="scss">
+@import "@/assets/styles/_variables.scss";
+
+.home {
+  width: 80vw;
+}
+</style>
